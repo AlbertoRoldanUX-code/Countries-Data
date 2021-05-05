5cf077ba41eec129f31489b5b9a82fd71ed61206
Alberto Roldán <a-f-r@hotmail.es>
2021-05-05 17:35:22 +0200

    Build card component

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+//Make AJAX call
 const request = new XMLHttpRequest();
 request.open('GET', 'https://restcountries.eu/rest/v2/name/spain');
 request.send();
@@ -12,4 +13,21 @@ request.send();
 request.addEventListener('load', function () {
   const [data] = JSON.parse(this.responseText);
   console.log(data);
+
+  //Build card component
+  const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>`;
+
+  console.log();
 });
