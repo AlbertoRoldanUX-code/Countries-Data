98dcff70734043d9326d99f0ff09999449df993d
Alberto Roldán <a-f-r@hotmail.es>
2021-05-07 17:20:19 +0200

    Call getCountryData when the user clicks on the button

--- script.js
+++ script.js
@@ -5,38 +5,6 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-// const getCountryData = function (country) {
-//   //Make AJAX call
-//   const request = new XMLHttpRequest();
-//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-//   request.send();
-
-//   request.addEventListener('load', function () {
-//     const [data] = JSON.parse(this.responseText);
-//     console.log(data);
-
-//     //Build card component
-//     const html = `
-//     <article class="country">
-//       <img class="country__img" src="${data.flag}" />
-//       <div class="country__data">
-//         <h3 class="country__name">${data.name}</h3>
-//         <h4 class="country__region">${data.region}</h4>
-//         <p class="country__row"><span>👫</span>${(
-//           +data.population / 1000000
-//         ).toFixed(1)}</p>
-//         <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//         <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-//       </div>
-//     </article>`;
-
-//     //Insert card component into page
-//     countriesContainer.insertAdjacentHTML('beforeend', html);
-
-//     countriesContainer.style.opacity = '1';
-//   });
-// };
-
 const renderCountry = function (data, className = '') {
   //Build card component
   const html = `
@@ -59,41 +27,6 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = '1';
 };
 
-// const getCountryAndNeighbour = function (country) {
-//   //Make AJAX call country 1
-//   const request = new XMLHttpRequest();
-//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-//   request.send();
-
-//   //Handle data when data arrives
-//   request.addEventListener('load', function () {
-//     const [data] = JSON.parse(this.responseText);
-//     console.log(data);
-
-//     //Render country 1
-//     renderCountry(data);
-
-//     //Get 1º neighbour of array of neighbours
-//     const [neighbour] = data.borders;
-
-//     //Don't display anything if country has no neighbours
-//     if (!neighbour) return;
-
-//     //Make AJAX call country 2
-//     const request2 = new XMLHttpRequest();
-//     request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
-//     request2.send();
-
-//     request2.addEventListener('load', function () {
-//       const data = JSON.parse(this.responseText);
-//       console.log(data);
-
-//       //Render country 2
-//       renderCountry(data, 'neighbour');
-//     });
-//   });
-// };
-
 const getCountryData = function (country) {
   //AJAX call country 1
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
@@ -112,8 +45,7 @@ const getCountryData = function (country) {
     .then(data => renderCountry(data, 'neighbour'));
 };
 
-getCountryData('spain');
-
-//Add functionality of loading the neighbour country
-
-//Render the neighbouring country with a promise
+// Call getCountryData when the user clicks on the button.
+btn.addEventListener('click', function () {
+  getCountryData('spain');
+});
